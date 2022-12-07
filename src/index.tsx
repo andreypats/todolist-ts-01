89ed82eec6c9f2ae58a259734de21ea20d586986
andreypats <andrey.it.work@gmail.com>
2022-12-08 01:05:46 +0300

    Lesson 16 is learning (1_29_00)

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import { store } from './state/store';
+import App from './app/App';
+import {Provider} from 'react-redux';
+import {store} from './app/store';
+import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
@@ -12,7 +13,9 @@ const root = ReactDOM.createRoot(
 
 root.render(
     <Provider store={store}>
-    <App />
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
     </Provider>
 );
 
